09109d84c9d44dd6b905f29523d809ea53894ccc
Tarun <tarun.prajapati912@gmail.com>
2023-02-11 11:45:57 +0530

    Initial commit

---
+++ index1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+     echo "<h3>Welcome to This PHP Website!</h3> <hr>This is First Page Using PHP and Xampp Environment, Nice to see you the all course of PHP.";
+
+ $name = "Tarun";
+ $surname = "Prajapati";
+
+ echo "<hr>Hi I am ","<b>$name $surname</b>";
+?>
+</body>
+</html>
